7ac154830795558dd5c1978c6bdb15c79835122f
tusharsrivastava2904 <tusharsrivastava2904@gmail.com>
2021-08-18 17:53:19 +0530

    final commit (bug_fix)

--- app/src/main/java/com/sritushar/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/sritushar/tic_tac_toe/MainActivity.java
@@ -1,102 +1,97 @@
 package com.sritushar.tic_tac_toe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    //0:0 ; 1:X ; 2:empty
-    boolean gameWin = false;
-    int winner = -1;
-    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winninState = {{0, 1, 2}, {0, 3, 6}, {0, 4, 8}, {1, 4, 7}, {2, 5, 8}, {3, 4, 5}, {2, 4, 6}, {6, 7, 8}};
-    int activePlayer = 0;
+    private final int o_tag = 0;
+    private final int x_tag = 1;
+    //O:0 X:1
+    int active_player = 0;
+    boolean game_active = true;
+    int[] game_state = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winning_states = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     private Button resetButton;
+    private TextView messageBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resetButton = findViewById(R.id.resetButton);
+        messageBox = findViewById(R.id.winTextLabel);
     }
 
+
     public void drop(View view) {
+        ImageView counter = (ImageView) view;
+        int tapped_counter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameWin == false) {
-            ImageView counter = (ImageView) view;
-            int tappedCounter = Integer.parseInt(counter.getTag().toString());
-            Log.i("TAG", "drop: " + tappedCounter);
-            gameState[tappedCounter] = activePlayer;
+        if (game_state[tapped_counter] == 2 && game_active) {
+            game_state[tapped_counter] = active_player;
+            counter.setTranslationY(-1500);
 
-            if (activePlayer == 0) {
+            if (active_player == o_tag) {
                 counter.setImageResource(R.drawable.o);
-                activePlayer = 1;
+                active_player = x_tag;
             } else {
                 counter.setImageResource(R.drawable.x);
-                activePlayer = 0;
+                active_player = o_tag;
             }
 
-            counter.setTranslationY(-2000);
-            counter.animate().translationYBy(2000).rotation(3600).setDuration(300);
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(150);
 
-            for (int[] winningPosition : winninState) {
-                if (gameState[winningPosition[0]] == gameState[winningPosition[1]]
-                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
-                        && gameState[winningPosition[0]] != 2) {
+            for (int[] winning_state : winning_states) {
+                if (game_state[winning_state[0]] == game_state[winning_state[1]]
+                        && game_state[winning_state[1]] == game_state[winning_state[2]]
+                        && game_state[winning_state[0]] != 2) {
 
-                    //someone has won
-                    winner = gameState[winningPosition[0]];
-                    if (winner == 0) {
-                        Log.i("TAG", "!!SOMEONE WON!! ");
-                        Toast.makeText(getBaseContext(), "Player O WINS!!!", Toast.LENGTH_LONG).show();
-                    } else if (winner == 1) {
-                        Log.i("TAG", "!!SOMEONE WON!! ");
-                        Toast.makeText(getBaseContext(), "Player X WINS!!!", Toast.LENGTH_LONG).show();
+                    game_active = false;
+
+                    //getting winner name for message!
+                    String winner = "";
+                    if (active_player == 1) {
+                        winner = getString(R.string.x_tag);
+                    } else {
+                        winner = getString(R.string.o_tag);
                     }
+                    String message = winner + " " + getString(R.string.has_won);
 
-                    TextView winText = (TextView) findViewById(R.id.winTextLabel);
-                    winText.setText("Player " + winner + " has WON!!");
-
-                    resetButton = (Button) findViewById(R.id.resetButton);
+                    //setting message and preparing screen for re-match
+                    messageBox.setText(message);
                     resetButton.setVisibility(View.VISIBLE);
-                    winText.setVisibility(View.VISIBLE);
-
-                    gameWin = true;
-
+                    messageBox.setVisibility(View.VISIBLE);
                 }
             }
-        } else {
-            Toast.makeText(this, "!!RESET TO BEGIN AGAIN!!", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void reset(View view) {
-
-        resetButton = (Button) findViewById(R.id.resetButton);
-        TextView winText = (TextView) findViewById(R.id.winTextLabel);
         resetButton.setVisibility(View.GONE);
-        winText.setVisibility(View.GONE);
+        messageBox.setVisibility(View.GONE);
 
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
-        for(int i=0; i<gridLayout.getChildCount(); i++) {
-           ImageView counter = (ImageView) gridLayout.getChildAt(i);
-           counter.setImageDrawable(null);
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
         }
 
-        //0:0 ; 1:X ; 2:empty
-        gameWin = false;
-        winner = -1;
-        for(int i: gameState) {
-            gameState[i] = 2;
+        for (int i = 0; i < game_state.length; i++) {
+            game_state[i] = 2;
         }
-        int activePlayer = 0;
+
+        active_player = 0;
+
+        game_active = true;
 
     }
 }
